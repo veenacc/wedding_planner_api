a0be2c66879e85a41d22880e32faef212e5b33b6
veenacc <veena.chenicheri@gmail.com>
2024-07-10 12:29:54 -0700

    implemented show test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -13,4 +13,13 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Event.count, data.length
   end
+
+   # test for event show
+  test "show" do
+    get "/weddings/#{Event.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "description", "address", "image_url","start_time", "end_time"], data.keys
+  end
 end
