8df5fec4f23cf38b5e2390658f7ed9484f6b9bd9
veenacc <veena.chenicheri@gmail.com>
2024-07-11 22:18:44 -0700

    added condition for update action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,16 +14,22 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find_by(id: params[:id])
-    @event.update(
-      name: params[:name] || @event.name,
-      description: params[:description] || @event.description,
-      address: params[:address] || @event.address,
-      image_url: params[:image_url] || @event.image_url,
-      user_id: params[:user_id] || @event.user_id,
-      start_time: params[:start_time] || @event.start_time,
-      end_time: params[:end_time] || @event.end_time,
-    )
-    render :show
+    if current_user
+      @event.update(
+        name: params[:name] || @event.name,
+        description: params[:description] || @event.description,
+        address: params[:address] || @event.address,
+        image_url: params[:image_url] || @event.image_url,
+        # user_id: params[:user_id] || @event.user_id,
+        user_id: current_user.id,
+        start_time: params[:start_time] || @event.start_time,
+        end_time: params[:end_time] || @event.end_time,
+      )
+      render :show
+    else
+      render json: {message: "not authorized"}
+    end
+    
   end
 
 end
